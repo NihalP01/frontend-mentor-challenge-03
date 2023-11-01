8820d9a1d0268ba7b1f79ecff620b5e2830e3205
Nihalp01 <nihalburagohain02@gmail.com>
2023-11-02 00:12:03 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function openTab(e, featureName) {
+  var tabContent, tabLink;
+  tabContent = document.getElementsByClassName('tab-content');
+  //Hide all the elements with class="tab-content"
+  for (let i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = 'none';
+  }
+  //Hide all the elements with class="tab-link"
+  tabLink = document.getElementsByClassName('tablink');
+  for (let i = 0; i < tabLink.length; i++) {
+    tabLink[i].className = tabLink[i].className.replace(
+      ' active',
+      ''
+    );
+  }
+  
+  document.getElementById(featureName).style.display = 'block';
+  e.currentTarget.className += ' active';
+}
