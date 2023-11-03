a23dc09fd9eae88dd23cba816dc540eb20b00703
Nihalp01 <nihalburagohain02@gmail.com>
2023-11-03 21:11:57 +0530

    final changes

--- script.js
+++ script.js
@@ -8,45 +8,27 @@ function openTab(e, featureName) {
   //Hide all the elements with class="tab-link"
   tabLink = document.getElementsByClassName('tablink');
   for (let i = 0; i < tabLink.length; i++) {
-    tabLink[i].className = tabLink[i].className.replace(
-      ' active',
-      ''
-    );
+    tabLink[i].className = tabLink[i].className.replace(' active', '');
   }
 
   document.getElementById(featureName).style.display = 'flex';
   e.currentTarget.className += ' active';
 }
 
+// To default open the first feature
 document.getElementById('defaultOpen').click();
 
-// document.addEventListener('DOMContentLoaded', (event) => {
-//   var acc = document.getElementsByClassName('accordion-box');
-//   var i;
 
-//   for (i = 0; i < acc.length; i++) {
-//     acc[i].addEventListener('click', function () {
-//       // var panel = this.nextElementSibling;
-//       var panel = document.getElementsByClassName('panel');
-//       if (panel[0].style.display === 'flex') {
-//         panel[0].style.display = 'none';
-//       } else {
-//         panel[0].style.display = 'flex';
-//       }
-//     });
-//   }
-// });
+const accordionHeaders = document.querySelectorAll(
+  '.accordion-box .accordion-header'
+);
+const panels = document.querySelectorAll('.accordion-box .panel');
+const arrows = document.querySelectorAll('.accordion-box .btn-arrow');
 
-var acc = document.getElementsByClassName('accordion-box');
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener('click', function () {
-    var panel = document.getElementsByClassName('panel');
-    if (panel.style.display === 'block') {
-      panel.style.display = 'none';
-    } else {
-      panel.style.display = 'block';
-    }
+accordionHeaders.forEach((header, index) => {
+  header.addEventListener('click', function () {
+    panels[index].style.display =
+      panels[index].style.display === 'block' ? 'none' : 'block';
+    arrows[index].classList.toggle('rotate');
   });
-}
+});
